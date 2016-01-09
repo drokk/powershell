952be2d689b461e5982b0fe20f77fd8229b04fca
Mathias Hansson <drokkie@gmail.com>
2016-01-08 16:35:19 -0800

    paramter support, remote command execution..

--- psssh/psssh.ps1
+++ psssh/psssh.ps1
@@ -1,31 +1,50 @@
+Param (
+    $key="null", #ssh key 
+    $p=22, #port 
+    $l, #login name
+    $i, #key file  
+    [parameter(position=0)] $hostname,
+    [parameter(position=1)] $command
+)
+
+if (!$hostname)
+{
+    exit ;
+}
+
+#check if Ssh-Sessions is installed 
 if (Get-Module -ListAvailable SSH-Sessions)
     {
         Import-Module SSH-Sessions
     } else {
         Write-Host "please install SSH-Sessions in $Env:PSModulePath"
     }
-#param {
-#    $key #ssh key 
-#    $l #login name 
-#}
+    
 
-[string] $hostname = $args[0]
-
+# check if the user used username@hostname notation in the $hostname variable or used -l username if not ask for username 
 if ($hostname -like "*@*")
     {
         $options = $hostname.Split("@")
         $username = $options[0]
         $hostname = $options[1]
+    } elseif ($l) {
+        $username = $l
+    } else {
+        $username = Read-Host "what's your username"
     }
     
 $output = Resolve-DnsName $hostname -ErrorVariable notvalid -ErrorAction SilentlyContinue
 
 if ($notvalid)
-    {Write-Host $hostname is a valid hostname. ; exit }
+    {Write-Host $hostname is a not valid hostname. ; exit }
+
+New-SshSession -ComputerName $hostname -Username $username
+
+if (!$command)
+    {Enter-SshSession -ComputerName $hostname} 
+    else
+    {Invoke-SshCommand -ComputerName $hostname -Command $command}
 
 
-New-SshSession -ComputerName $hostname -Username $username 
-
-Enter-SshSession -ComputerName $hostname
 
 Remove-SshSession -ComputerName $hostname
